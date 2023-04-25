51195111cc21464543a53629590a7cfe138d1ec4
Robert <turbobert-github@mailbox.org>
2023-04-25 10:59:12 +0200

    ...

--- src/mailhook/__main__.py
+++ src/mailhook/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import configparser
 import rich.progress
 
@@ -13,11 +14,18 @@ print(CFG)
 
 import subprocess
 
-list_emlx = subprocess.check_output('find %s -name "*.emlx" -print0' % CFG['dir'], shell=True, universal_newlines=True).strip().split(chr(0))
+list_emlx = subprocess.check_output('find %s -name "*.emlx" -print0' % CFG['dir'], shell=True, universal_newlines=True).strip().split(chr(0))[0:-1]
 
 for x in rich.progress.track(range(len(list_emlx)), "Loading all emlx files"):
     with open(list_emlx[x], 'r') as f:
         try:
             lines = f.read().strip().split("\n")
+            lines = lines[1:]
+            killstart = len(lines)-1
+            while killstart > 0 and not lines[killstart].startswith("<?xml version"):
+                killstart-=1
+            lines = lines[0:killstart]
+            print(lines)
+            sys.exit(0)
         except:
             pass
