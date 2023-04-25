4d36aa14c096ceda5be52b6bbfacf97f0dcff050
Robert <turbobert-github@mailbox.org>
2023-04-25 09:43:24 +0200

    ...

--- src/mailhook/__main__.py
+++ src/mailhook/__main__.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+import os
+import configparser
+
+cp = configparser.ConfigParser()
+cp.read_string("[_]" . open(os.path.expanduser("~/.mailhookrc"), "r").read())
+
+CFG = dict(cp["_"])
+print(CFG)
