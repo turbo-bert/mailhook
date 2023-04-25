786a166007f92aec12e5179f146efb3446bff793
Robert <turbobert-github@mailbox.org>
2023-04-25 09:56:02 +0200

    ...

--- src/mailhook/__main__.py
+++ src/mailhook/__main__.py
@@ -8,3 +8,10 @@ cp.read_string("[_]\n" + open(os.path.expanduser("~/.mailhookrc"), "r").read())
 
 CFG = dict(cp["_"])
 print(CFG)
+
+
+import subprocess
+
+list_emlx = subprocess.check_output('find %s -name "*.emlx" -print0' % CFG['dir'], shell=True, universal_newlines=True).split(chr(0))
+
+print(list_emlx)
